2acc7bf05189fb615584c8d837a95c0efc2ff86a
Coxlee Anthony Hubilla <coxleehubilla@gmail.com>
2021-03-02 22:42:06 +0800

    fix product submission layout

--- components/home/product-submit/ProductSubmission.vue
+++ components/home/product-submit/ProductSubmission.vue
@@ -1,6 +1,31 @@
 <template>
   <v-container>
     <h1 class="mb-3 display-1">Submit a Product</h1>
+    <v-row>
+      <v-col cols="3">
+        <v-text-field label="Product Name">
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3">
+        <v-autocomplete
+          label="Product Type"
+        ></v-autocomplete>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3">
+        <v-autocomplete
+          label="Manufacturer"
+        ></v-autocomplete>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn color="primary">Submit Product</v-btn>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
